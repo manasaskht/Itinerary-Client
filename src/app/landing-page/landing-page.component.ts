8d762de59185cdd43c1a5db85066e1749e64d925
Nikhil Tyagi <nikhil.tyagi62@gmail.com>
2019-02-08 21:51:39 -0400

    Timeline and Screenshots

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-    constructor() { }
+    features = [];
+
+    constructor() {
+        this.features = [
+            { image: 'assets/imgs/videochat.png', name: 'Video Call' },
+            { image: 'assets/imgs/map.PNG', name: 'Real-time map' },
+            { image: 'assets/imgs/timeline.png', name: 'Interactive Timeline & Suggestions' },
+        ];
+    }
 
     ngOnInit() {
     }
