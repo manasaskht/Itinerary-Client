8b9b6f22f7a86ad4169d52e403102013279c642f
Aman Arya <aman.arya@dal.ca>
2019-03-22 17:42:50 -0300

    Completed: Fetching the friends list

--- src/app/dashboard/create-itinerary/friend.ts
+++ src/app/dashboard/create-itinerary/friend.ts
@@ -1,4 +1,8 @@
 export class Friend {
     email: string;
     name: string;
+    constructor(object) {
+        this.email = object.emailAddress;
+        this.name = object.firstName + ' ' + object.lastName;
+    }
 }
--- src/app/dashboard/create-itinerary/friends.service.ts
+++ src/app/dashboard/create-itinerary/friends.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { serverUrls } from '../../shared/utilities/app.urls.helper';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addFriend(emailId: string) {
+    let url = serverUrls.addFriend;
+    let body = {
+      emailId
+    };
+
+    return this.http.post(url, body);
+  }
+
+  getFriends() {
+    let url = serverUrls.getFriends;
+    let body = {};
+    return this.http.get(url, body);
+  }
 }
