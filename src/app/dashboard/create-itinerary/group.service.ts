585e6a0aba9726041584b2459e39b4b53ecbecd0
Aman Arya <aman.arya@dal.ca>
2019-03-30 17:33:45 -0300

    Completed: Add and get members of group

--- src/app/dashboard/create-itinerary/group.service.ts
+++ src/app/dashboard/create-itinerary/group.service.ts
@@ -3,27 +3,46 @@ import { HttpClient } from '@angular/common/http';
 import { serverUrls } from 'src/app/shared/utilities/app.urls.helper';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GroupService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  // Create a group for an itinerary. One itinerary has exactly one group associated with it
-  createGroup(id: string, title: string) {
-    let url = serverUrls.createGroup;
-    let body = {
-      id,
-      title
+    // Create a group for an itinerary. One itinerary has exactly one group associated with it
+    createGroup(id: string, title: string) {
+        const url = serverUrls.createGroup;
+        const body = {
+            id,
+            title
+        };
+        return this.http.post(url, body);
     }
-    return this.http.post(url, body);
-  }
 
-  // // Get a list of groups for currently logged-in user
-  getGroups() {
-    let url = serverUrls.getGroups;
-    // The server already has the logged-in users information so nothing needs to be passed in body
-    let body = {};
-    return this.http.get(url, body);
-  }
+    // Get a list of groups for currently logged-in user
+    getGroups() {
+        const url = serverUrls.getGroups;
+        // The server already has the logged-in users information so nothing needs to be passed in body
+        const body = {};
+        return this.http.get(url, body);
+    }
+
+    // Add a member to a group
+    addMember(friendId, itineraryId) {
+        const url = serverUrls.addMember;
+        const body = {
+            friendId,
+            itineraryId
+        };
+        return this.http.post(url, body);
+    }
+
+    // Get members of a group
+    getMembers(groupName) {
+        const url = serverUrls.getMembers;
+        const params = {
+            groupName
+        };
+        return this.http.get(url, { params });
+    }
 }
