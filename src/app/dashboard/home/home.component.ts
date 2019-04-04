4b6d9530fbd64b1d534b55c34d7d67e2bf31ddf2
Nikhil Tyagi <nikhil.tyagi62@gmail.com>
2019-04-03 21:38:43 -0300

    Home Changes

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -26,9 +26,27 @@ export class HomeComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+        this.listItineraries();
+    }
+
+    getImage() {
+        let urls = [
+            'https://www.strollingoftheheifers.com/wp-content/uploads/2012/12/Know-Your-Forester.jpg',
+            'https://hmhps.ca/images/locations/citadel-hill/citadel-hill-1.jpg',
+            'https://www.gannett-cdn.com/presto/2018/07/27/USAT/9c49add3-e5fa-4aaa-8af3-458fd0fa186f-italy-venice-gondolas-051018-az.jpg?crop=1799,1012,x0,y0&width=3200&height=1680&fit=bounds',
+            'https://d39gusjpdm7p1o.cloudfront.net/data/layout_grouping/static_page_step/20784/a330628091ede7eb1548d6cda58e0357.jpg?ver=1477297804',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ2VXfrz8eY0L31238Z5C3phG-e3EbQb_qm9ZsRwuUJZxFo4LwKA',
+            'https://blockchaininvestio-c007.kxcdn.com/wp-content/uploads/2018/07/India-bitcoin.jpg',
+            'http://www.traveller.com.au/content/dam/images/h/0/v/z/w/u/image.related.articleLeadwide.620x349.h166kx.png/1539216554259.jpg',
+            'https://hmhps.ca/images/locations/citadel-hill/citadel-hill-1.jpg',
+        ];
+        return urls[Math.floor(Math.random() * urls.length)];
+    }
+
+    listItineraries() {
         this.itineraryService.listItineraries().subscribe((res: Array<any>) => {
             this.upcomingTrips = res;
-        })
+        });
     }
 
     getPlaces(places: Array<string>) {
